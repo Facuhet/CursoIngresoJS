978910a04c7500edace9da9c5273d0be3f110cbe
alumno <alumno@utnfra.com.ar>
2020-02-13 22:10:09 -0300

    Terminado

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -3,7 +3,29 @@ function mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
-//alert (mesDelAño);
+    switch(mesDelAño)
+    {
+        case "Abril":
+         alert("Tiene 30 dias")
+            break;
+        case "Junio":
+        alert("Tiene 30 dias")
+            break;
+        case "Septiembre":
+        alert("Tiene 30 dias")
+            break;
+        case "Noviembre":
+        alert("Tiene 30 dias")
+            break;
+        case "Febrero":
+        alert("Tiene 29 dias")
+            break;
+        default:
+        alert("tiene 31 dias");
+            break;
+        
+    }
+
 	
 	
 
