dd8431af2e682c85f9b05fdd0833b9091670e673
Facundo <facuhet@gmail.com>
2020-02-16 18:00:25 -0300

    Terminado

--- 8-TPs/jsAdivinaElNumero1.js
+++ 8-TPs/jsAdivinaElNumero1.js
@@ -7,20 +7,50 @@ secreto del 1 al 100, en la pantalla del juego
 “Usted es un ganador!!! y en solo X intentos”.
 de no ser igual se debe informar si “falta…”  para llegar al número secreto  o si “se pasó…”  del número secreto.
 */
-var numeroSecreto; 
-var contadorIntentos;
+    var numeroSecreto; 
+    var contadorIntento;
+    contadorIntento = 1;
 
 function comenzar()
 {
 	//Genero el número RANDOM entre 1 y 100
 	 
-		//alert(numeroSecreto );
+    //alert(numeroSecreto );}
+    numeroSecreto = Math.floor(Math.random()*101);
+    
+    
 	
 
 }
 
+    var numeroIngresado;
+
 function verificar()
 {
-	
+    numeroIngresado = document.getElementById("numero").value;
+    if(contadorIntento == 10){
+      document.getElementById("intentos").value = contadorIntento = 0;
+      alert("El juego termino");
+
+    }else {
+        if(numeroIngresado < numeroSecreto){
+          alert("El numero que pense es mayor")
+          document.getElementById("intentos").value = contadorIntento ++;
+          
+        }else {
+          if(numeroIngresado > numeroSecreto){
+            alert("El numero que pense es menor");
+            document.getElementById("intentos").value = contadorIntento ++;
+          }else {
+
+            if(numeroIngresado == numeroSecreto){
+              alert("Has ganado y en solo: "+ contadorIntento+ " intentos");
+              document.getElementById("intentos").value = contadorIntento ++;
+            }
+
+          }
+        }
+
+    }
 	
 }
