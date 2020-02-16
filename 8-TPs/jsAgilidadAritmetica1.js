9980e97f092ce19a0d0c1fc7d3a9713ba95fcb59
Facundo <facuhet@gmail.com>
2020-02-16 20:13:21 -0300

    Terminado

--- 8-TPs/jsAgilidadAritmetica1.js
+++ 8-TPs/jsAgilidadAritmetica1.js
@@ -6,15 +6,58 @@ y una de las cuatro operaciones básicas (suma, resta,
  el resultado de la operación y presionar el botón Aceptar.
 se debe informar si el resultado es el correcto o no.
 */
-var respuesta;
+    var respuesta;
+    var numeroAleatorioUno;
+    var numeroAleatorioDos;
+    var operadorMatematico;
+    var respuestaUsuario;
+    var operadorRandom;
 function comenzar()
 {
+    numeroAleatorioUno = Math.floor(Math.random()*10+1);
+    numeroAleatorioDos = Math.floor(Math.random()*10+1);
+    operadorRandom = Math.floor(Math.random()*4 + 1);
+    
 
-	
+    switch(operadorRandom){
+        case 1:
+                 operadorMatematico = "+";
+                 respuesta = numeroAleatorioUno + numeroAleatorioDos;
+        break;
+
+        case 2:
+                operadorMatematico = "-";
+                respuesta = numeroAleatorioUno - numeroAleatorioDos;
+        break;
+       
+        case 3:
+                operadorMatematico = "*";
+                respuesta = numeroAleatorioUno * numeroAleatorioDos;
+        break;
+
+        case 4:
+                operadorMatematico = "/";
+                respuesta = numeroAleatorioUno / numeroAleatorioDos;
+        break;
+
+    }
+    
+    document.getElementById("Operador").value = operadorMatematico;
+    document.getElementById("PrimerNumero").value = numeroAleatorioUno
+    document.getElementById("SegundoNumero").value = numeroAleatorioDos
+    
 
 }//FIN DE LA FUNCIÓN
 function Responder()
 {
-	
+    respuestaUsuario = document.getElementById("Respuesta").value;
+
+	if(respuestaUsuario == respuesta){
+        alert("!Correcto¡");
+    }else if(respuestaUsuario != respuesta){
+        alert("!Incorrecto¡");
+    }
+        
+    
 
 }//FIN DE LA FUNCIÓN
